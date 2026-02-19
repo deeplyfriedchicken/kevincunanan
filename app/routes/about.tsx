2609cd08ed43eededa366dcb59f45ab869c0e31f
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2026-02-18 23:37:20 -0800

    updated about.tsx

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -11,16 +11,14 @@ export default function About() {
 	return (
 		<main className="px-[2rem] md:px-[4rem] py-[3rem] md:py-[5rem]">
 			<p className="text-theme-text text-[1.125rem] md:text-[1.25rem] font-light leading-relaxed max-w-[48rem]">
-				I'm a software engineer, that affirms in team-driven development
-				and a desire to continuously find and implement the best
-				practices.
+				Frontend engineer, AI supervisor, Full-Stack Developer
 			</p>
 
 			<div className="flex flex-col sm:flex-row gap-[2rem] mt-[4rem]">
 				<div className="flex items-center gap-[0.75rem]">
 					<MapPin className="w-[2.5rem] h-[2.5rem] text-theme-text" />
 					<span className="text-theme-text-light text-[1.875rem] md:text-[2.25rem] font-light">
-						Los Angeles
+						Bay Area, CA | Hybrid | Remote
 					</span>
 				</div>
 				<div className="flex items-center gap-[0.75rem]">
@@ -33,27 +31,11 @@ export default function About() {
 
 			<div className="mt-[4rem] max-w-[48rem] space-y-[1.5rem] text-theme-text font-light leading-relaxed text-[1rem]">
 				<p>
-					I believe that the most important skill for a developer is
-					the ability to learn while also having the willingness to
-					adapt. When developing an application, I don't mindlessly
-					develop; instead, I code and build features that make sense
-					and discuss with others on those features that don't.
-				</p>
+					While I am a full-stack developer at heart, I have 6+ years of focused experience working in frontend engineering; sprinkled in there is regular work with CI pipelines, API contracts, and UI/UX.</p>
 				<p>
-					While the majority of my knowledge has been centered around
-					traditional web development, I'm currently trying personal
-					projects on mobile and desktop platforms using frameworks
-					like Swift and React Native in conjunction with GraphQL.
+					Work is not everything though. I love sports 🏐, climbing 🧗🏽‍♂️, snow 🎿, and many games🕹️.
 				</p>
-				<p>
-					However, my life extends beyond software engineering and at
-					the end of the day I look for ways to bond with others
-					through activities like competing in super smash bros
-					parties, playing volleyball, or rock climbing; I think having
-					that activity and engagement is super important part of
-					work-life balance.
-				</p>
-				<p>I'm also a huge fan cats and other fluffy animals.</p>
+				<p>If you couldn't tell, I like cats.</p>
 			</div>
 		</main>
 	);
