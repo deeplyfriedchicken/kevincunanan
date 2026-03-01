8792d3abd53f63ffdd23e54d35b7854b5d687812
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2026-02-28 23:18:44 -0800

    refactored projects page

--- app/components/TechTag.tsx
+++
@@ -1,7 +0,0 @@
-export function TechTag({ label }: { label: string }) {
-	return (
-		<span className="inline-block border border-theme-text text-theme-text px-[1rem] py-[0.25rem] rounded-full text-[0.875rem] font-merriweather-sans">
-			{label}
-		</span>
-	);
-}
