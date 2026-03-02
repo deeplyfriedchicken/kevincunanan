9b861527cb270301ebe5b9e76fe0b157f84e38f3
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2026-03-02 10:57:57 -0800

    color switcher fix

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -75,7 +75,7 @@ export function ThemeSwitcher() {
 			onMouseLeave={handleMouseLeave}
 			onClick={handleContainerClick}
 			onKeyDown={handleContainerClick}
-			className="fixed bottom-10 left-0 right-0 justify-center flex-row gap-[15px] md:bottom-8 md:right-8 md:left-auto md:flex-col md:items-center md:gap-3 flex z-50 border-none p-0 m-0"
+			className="fixed bottom-[80px] left-[31.5px] justify-center flex-col gap-[15px] md:bottom-8 md:right-8 md:left-auto md:flex-col md:items-center md:gap-3 flex z-50 border-none p-0 m-0"
 			style={{ "--dot-count": themes.length } as React.CSSProperties}
 		>
 			{themes.map(({ slug, primaryColor }, index) => (
